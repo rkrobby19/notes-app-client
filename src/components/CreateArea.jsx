67cb09971078608a367888e67936c6d23a75c703
rkrobby19 <rizkyrobby19@gmail.com>
2023-02-20 21:07:43 +0700

    Add post method to api server

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,16 +1,54 @@
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
+import NoteServices from "../services/notes";
 
 const CreateArea = () => {
+  const [tittle, setTittle] = useState();
+  const [body, setBody] = useState();
+
+  const tittleChangeHandler = (event) => {
+    setTittle(event.target.value);
+  };
+  const bodyChangeHandler = (event) => {
+    setBody(event.target.value);
+  };
+
+  const resetState = () => {
+    setTittle();
+    setBody();
+  };
+
+  const submitHandler = async () => {
+    const resp = await NoteServices.addNotes(tittle, body);
+    const data = await resp.json();
+    if (data.note._id) {
+      alert(`Successfully add note`);
+      resetState();
+    } else {
+      alert(`Failed to add note`);
+      resetState();
+    }
+  };
+
   return (
     <Form className="create-area">
-      <Form.Group className="mb-3" controlId="title">
-        <Form.Control type="text" placeholder="title" />
+      <Form.Group className="mb-3" controlId="tittle">
+        <Form.Control
+          type="text"
+          placeholder="Tittle"
+          onChange={tittleChangeHandler}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="body">
-        <Form.Control as="textarea" rows={3} placeholder="your notes .." />
+        <Form.Control
+          as="textarea"
+          rows={3}
+          placeholder="Your notes ..."
+          onChange={bodyChangeHandler}
+        />
       </Form.Group>
-      <Button onClick={() => alert(`clicked`)}>Add</Button>
+      <Button onClick={submitHandler}>Add</Button>
     </Form>
   );
 };
---
+++ src/services/notes.js
@@ -0,0 +1,16 @@
+const URI = "https://simple-notes-app.fly.dev/api/notes";
+
+class NoteServices {
+  static addNotes = async (tittle, body) => {
+    const resp = await fetch(URI, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ tittle, body }),
+    });
+    return resp;
+  };
+}
+
+export default NoteServices;
